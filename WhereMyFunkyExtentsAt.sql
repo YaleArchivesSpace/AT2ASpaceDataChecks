a280107df23814fa3cda5f86958912723aca1bab
Maureen Callahan <maureen.callahan@gmail.com>
2015-05-19 10:39:18 -0400

    Create WhereMyFunkyExtentsAt.sql

---
+++ WhereMyFunkyExtentsAt.sql
@@ -0,0 +1,16 @@
+SELECT 
+    CONCAT(r.resourceIdentifier1,
+            ' ',
+            LPAD(r.resourceIdentifier2, 4, '00')) 'Collection',
+    r.title 'Collection Title',
+    series.subdivisionIdentifier 'Series/Accession Number',
+    series.title 'Series Title',
+    rc.title 'Component Title',
+    rc.dateExpression 'Component Date'
+FROM
+    ResourcesComponents rc 
+        INNER JOIN
+    Resources r ON r.resourceId = GETRESOURCEFROMCOMPONENT(rc.resourceComponentId)
+        LEFT OUTER JOIN
+    ResourcesComponents series ON GETTOPCOMPONENT(rc.resourceComponentId) = series.resourceComponentID
+    where rc.extentType=''
